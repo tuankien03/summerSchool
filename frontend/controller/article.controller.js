7f05e7ad85c3a6f7e51015b5e1b8ba3293c3a756
tuankien03 <nguyentuankien03@gmail.com>
2024-06-13 23:09:59 +0700

    add countdown timer

--- frontend/controller/article.controller.js
+++ frontend/controller/article.controller.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 class ArticleController {
     showArticles = async (req, res, next) => {
         const articles = (await axios.get('http://localhost:3000/v1/api/articles')).data;
